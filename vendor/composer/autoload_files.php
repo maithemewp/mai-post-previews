179b98fd4d335eab0fe23ad9114d0fcdb13ef6f6
JiveDig <jivetheuniversal@gmail.com>
2023-10-06 15:16:33 -0400

    update dependencies

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -7,5 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     '09fc349b549513bf7f4291502426f919' => $vendorDir . '/embed/embed/src/functions.php',
-    '9b77ddcfb14408a32f5aaf74e0a11694' => $vendorDir . '/yahnis-elsts/plugin-update-checker/load-v5p1.php',
+    'efd9d646f43178e7ba3f07758c02ce1d' => $vendorDir . '/yahnis-elsts/plugin-update-checker/load-v5p2.php',
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,7 +8,7 @@ class ComposerStaticInit9c32fd546e05eca7e1da9c732d5acf48
 {
     public static $files = array (
         '09fc349b549513bf7f4291502426f919' => __DIR__ . '/..' . '/embed/embed/src/functions.php',
-        '9b77ddcfb14408a32f5aaf74e0a11694' => __DIR__ . '/..' . '/yahnis-elsts/plugin-update-checker/load-v5p1.php',
+        'efd9d646f43178e7ba3f07758c02ce1d' => __DIR__ . '/..' . '/yahnis-elsts/plugin-update-checker/load-v5p2.php',
     );
 
     public static $prefixLengthsPsr4 = array (
