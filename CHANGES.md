0c239401fb26ea16a9118b893283b7a8feb871a0
JiveDig <jivetheuniversal@gmail.com>
2023-10-06 15:23:35 -0400

    0.2.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.2.0 (10/6/23)
+* Fixed: URL and description sometimes causing errors when fetching meta from url.
+* Changed: URL now uses exact url from block settings.
+
 ## 0.1.2 (6/8/23)
 * Fixed: Make sure 'host' is returned when parsing url.
 
--- mai-post-previews.php
+++ mai-post-previews.php
@@ -4,7 +4,7 @@
  * Plugin Name:     Mai Post Previews
  * Plugin URI:      https://bizbudding.com
  * Description:     Show a preview embed of external posts via meta data.
- * Version:         0.1.2
+ * Version:         0.2.0
  *
  * Author:          BizBudding, Mike Hemberger
  * Author URI:      https://bizbudding.com
@@ -92,7 +92,7 @@ final class Mai_Post_Previews_Plugin {
 	private function setup_constants() {
 		// Plugin version.
 		if ( ! defined( 'MAI_POST_PREVIEWS_VERSION' ) ) {
-			define( 'MAI_POST_PREVIEWS_VERSION', '0.1.2' );
+			define( 'MAI_POST_PREVIEWS_VERSION', '0.2.0' );
 		}
 
 		// Plugin Folder Path.
